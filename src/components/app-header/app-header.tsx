9063a31acee90924ecef7bbc5ab712aa969bc126
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-05-27 17:28:33 +0300

    Sprint1/Step1 Отформатированы, подредактированы некоторые файлы, а также добавлены новые (включая тестовый компонент)

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app-header.scss';
+
+function AppHeader() {
+  return (
+    <div className="app-header">
+      <span>Hello World!</span>
+    </div>
+  );
+}
+
+export default AppHeader;
