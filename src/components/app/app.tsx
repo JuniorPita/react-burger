c70b2adb4436cbb816652de8a8b8152e95284be1
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-05-28 14:17:58 +0300

    Перенесён ещё один файл в папку components

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.scss';
+import AppHeader from '../app-header/app-header';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
