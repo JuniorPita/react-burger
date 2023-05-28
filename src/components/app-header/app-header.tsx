6e8862184bc456fbfe2c0ac6aa03a7c76211bcfd
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-05-28 16:14:18 +0300

    Внесены промежуточнные изменения в AppHeader

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import './app-header.scss';
+import appHeaderStyles from "./app-header.module.scss";
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
-function AppHeader() {
+function AppHeader(): JSX.Element {
   return (
-    <div className="app-header">
-      <span>Hello World!</span>
+    <div className={appHeaderStyles.header}>
+      <nav className={appHeaderStyles.nav}>
+        <ul className={appHeaderStyles.nav__list}>
+          <li className={`${appHeaderStyles.nav__listItem} ${appHeaderStyles.nav__firstBlock}`}>
+            <BurgerIcon type="primary" />
+            <p className="text_type_main-default ml-2">Конструктор</p>
+          </li>
+
+          <li className={`${appHeaderStyles.nav__listItem} ${appHeaderStyles.nav__secondBlock}`}>
+            <ListIcon type="secondary" />
+            <a href="#" className="text text_type_main-default text_color_inactive ml-2">Лента заказов</a>
+          </li>
+
+          <li className={`${appHeaderStyles.nav__listItem} ${appHeaderStyles.nav__logo}`}>
+            <a href="#">
+              <Logo />
+            </a>
+          </li>
+
+          <li className={`${appHeaderStyles.nav__listItem} ${appHeaderStyles.nav__profile}`}>
+            <ProfileIcon type="secondary" />
+            <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</p>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
