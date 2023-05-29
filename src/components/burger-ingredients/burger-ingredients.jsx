df4a57845853761b13f024cd3957187860fc910e
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-05-29 14:41:23 +0300

    Изменены старые компоненты и их стили

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -42,9 +42,9 @@ const BurgerIngredients = () => {
         <ul
           className={burgerIngredientsStyles.burgerIngredients__components_list}
         >
-          {data.map((obj) => {
-            if (obj.type === "bun") {
-              return <BurgerIngredient key={obj._id} {...obj} />;
+          {data.map((object) => {
+            if (object.type === "bun") {
+              return <BurgerIngredient key={object._id} {...object} />;
             }
           })}
         </ul>
@@ -53,9 +53,9 @@ const BurgerIngredients = () => {
         <ul
           className={burgerIngredientsStyles.burgerIngredients__components_list}
         >
-          {data.map((obj) => {
-            if (obj.type === "sauce") {
-              return <BurgerIngredient key={obj._id} {...obj} />;
+          {data.map((object) => {
+            if (object.type === "sauce") {
+              return <BurgerIngredient key={object._id} {...object} />;
             }
           })}
         </ul>
@@ -64,9 +64,9 @@ const BurgerIngredients = () => {
         <ul
           className={burgerIngredientsStyles.burgerIngredients__components_list}
         >
-          {data.map((obj) => {
-            if (obj.type === "main") {
-              return <BurgerIngredient key={obj._id} {...obj} />;
+          {data.map((object) => {
+            if (object.type === "main") {
+              return <BurgerIngredient key={object._id} {...object} />;
             }
           })}
         </ul>
