fcc9d05418e95a1a6a135b2d5e2019598c361945
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-05-29 14:41:50 +0300

    Новый компонент привязан в корневой (App)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import AppStyles from "./app.module.scss";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
 
       <main className={AppStyles.main}>
         <BurgerIngredients />
+        <BurgerConstructor />
       </main>
     </div>
   );
