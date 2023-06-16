933d5d75961a30525c3931b38bc4c5440ea20db4
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-06-16 21:13:42 +0300

    Изменён общий index.tsx файл

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
+/* Общие импорты */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
+import store from './services/store';
 
-const root = ReactDOM.createRoot(
+/* Стили */
+import './index.scss';
+
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
 
 reportWebVitals();
