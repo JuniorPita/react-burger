96bc926a10b0a3c7627756dda08ec11b16a0b098
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-07-29 16:00:55 +0300

    Изменён index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 import store from './services/store';
@@ -16,7 +17,9 @@ const root = createRoot(
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
