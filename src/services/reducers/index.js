d9e7fac40ed8b322aec6939d5f7a0e0e1c9f9f3c
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-07-29 16:02:00 +0300

    Добавлены и изменены редьюсеры

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -6,10 +6,12 @@ import { getIngredientsReducer } from "../reducers/get-ingredients";
 import { burgerConstructorReducer } from "./burger-constructor";
 import { ingredientDetailsReducer } from "./ingredient-details";
 import { orderReducer } from "./order";
+import { userReducer } from "./user";
 
 export const rootReducer = combineReducers({
   ingredients: getIngredientsReducer,
   burgerIngredients: burgerConstructorReducer,
   ingredientDetails: ingredientDetailsReducer,
   orderNumber: orderReducer,
+  user: userReducer,
 });
